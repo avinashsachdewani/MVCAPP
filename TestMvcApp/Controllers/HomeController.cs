40e4f6d05004826cab130c4deecfe48b726fd760
Avinash <avinash.sachdewani@motifworks.com>
2015-10-20 12:24:07 +0530

    Complete application commit.

---
+++ TestMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+namespace TestMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            try
+            {
+                Trace.TraceInformation("Displaying the Index page at " + DateTime.Now.ToLongTimeString());
+                Trace.TraceInformation("Checkin");
+                ViewBag.Message = "1";
+                int a = ViewBag.Message;
+            }
+            catch (Exception ex)
+            {
+                string err = ex.Message;
+                if (ex.InnerException != null)
+                {
+                    err += " Inner Exception: " + ex.InnerException.Message;
+                }
+                Trace.TraceError(err);
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
